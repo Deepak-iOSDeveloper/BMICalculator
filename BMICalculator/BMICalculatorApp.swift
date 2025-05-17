18e74e9aa45e39d2249560c3c351ba077938bb68
Deepak <deepakjgrt99@gmail.com>
2025-05-17 13:12:06 +0530

    Initial Commit

---
+++ BMICalculator/BMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorApp.swift
+//  BMICalculator
+//
+//  Created by Deepak Kumar Behera on 17/05/25.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMICalculatorTests/BMICalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorTests.swift
+//  BMICalculatorTests
+//
+//  Created by Deepak Kumar Behera on 17/05/25.
+//
+
+import Testing
+@testable import BMICalculator
+
+struct BMICalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
